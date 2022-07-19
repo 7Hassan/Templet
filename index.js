ab6a9f7256d85d45d9631db01c8f435fbc5d6b46
7Hassan <egoker1234@gmail.com>
2022-07-19 20:45:17 +0200

    JS File

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector("#menuclick").onclick=function (){
+    if (document.querySelector("#men").style.display=='none') {
+        document.querySelector("#men").style.display='flex';
+    }else {
+        document.querySelector("#men").style.display='none';
+    }
+}
